b7e65bc3cce26042d3fe10b3f5220b36210e5587
Barranco <barrancodylans@gmail.com>
2025-04-05 16:48:31 -0500

    Clase terminada

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -2,7 +2,13 @@ public class Decisiones {
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElplan = true;
-        double notaDeLaPelicula = 8.8;
-        System.out.println(fechaDeLanzamiento);
+        double notaDeLaPelicula = 8.2;
+        if (fechaDeLanzamiento >= 2022) {
+            System.out.println("Péliculas más populares");
+        }else {
+            System.out.println("Peliculas Retro que aun vale la pena ver ");
+        }
+        }
+
     }
-}
+
