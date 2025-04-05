5df3f77479a11ee86de867817d6821b8a6c71f77
Barranco <barrancodylans@gmail.com>
2025-04-05 17:15:13 -0500

    Clase terminada

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -1,12 +1,23 @@
 public class Decisiones {
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElplan = true;
+        boolean incluidoEnElplan = false;
         double notaDeLaPelicula = 8.2;
-        if (fechaDeLanzamiento >= 2022) {
-            System.out.println("Péliculas más populares");
+        String tipoPlan = "Plus";
+//        if (fechaDeLanzamiento >= 2022) {
+//            System.out.println("Péliculas más populares");
+//        }else {
+//            System.out.println("Peliculas Retro que aun vale la pena ver ");
+//        }
+//        if (incluidoEnElplan || tipoPlan.equals("Plus") ) {
+//            System.out.println("Disfruta de tu pelicula");
+//        }else {
+//            System.out.println("Pelicula no disponible en el plan");
+//        }
+        if (incluidoEnElplan && tipoPlan.equals("Plus") ) {
+            System.out.println("Disfruta de tu pelicula");
         }else {
-            System.out.println("Peliculas Retro que aun vale la pena ver ");
+            System.out.println("Pelicula no disponible en el plan");
         }
         }
 
