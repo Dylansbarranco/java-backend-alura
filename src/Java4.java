523e7a9d1af1037f331f7c15ce96c81f03e04ffb
Barranco <barrancodylans@gmail.com>
2025-04-11 15:41:51 -0500

    Condicionales

---
+++ src/Java4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Java4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Es segura tu contraseña");
+        String contraseña = sc.nextLine();
+
+        if (contraseña.length() >= 8) {
+
+         if (contraseña.contains("A") || contraseña.contains("E") || contraseña.contains("I") || contraseña.contains("O") || contraseña.contains("U")) {
+          if (contraseña.matches(".*\\d.*")) {
+              System.out.println("Tu contraseña es segura");
+                }
+            }
+
+            } else
+            System.out.println("Tu contraseña no es segura");
+        }
+
+    }
+
